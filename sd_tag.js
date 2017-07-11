1c262e6138b93683b0631966fbb5492b865072a5
Marco F. Cavaliere <marco.f.cavaliere@gmail.com>
2017-07-11 22:54:41 +0200

    Priority Fix & Performance

--- sd_tag.js
+++ sd_tag.js
@@ -4,6 +4,23 @@
 //
 //
 //
+const log_debug_color = "color: CornflowerBlue";
+const log_debug_error = "color: red";
+
+var pn = digitalData.page.pageInfo.pageName;
+var vb = digitalData.page.pageInfo.vehicleBrand;
+var vm = digitalData.page.pageInfo.vehicleModel;
+var di = digitalData.page.pageInfo.dealerID;
+var pt = "DDA"
+var oi = digitalData.page.pageInfo.transactionID;
+var rv = 1;
+
+
+var pn_onChangeFn = [];
+var vb_onChangeFn = [];
+var vm_onChangeFn = [];
+var di_onChangeFn = [];
+var oi_onChangeFn = [];
 
 function AdobeEverestTech__Tracking_Pixel(pageName) {
 	/* Adobe EverestTech Tag
@@ -12,11 +29,11 @@ function AdobeEverestTech__Tracking_Pixel(pageName) {
 	*/
     if ( ( typeof(pageName) != "undefined" ) && ( pageName != "" ) ) {
 	    var AdobeEverestTech__Tracking_Pixel = new Image(1, 1);
-		AdobeEverestTech__Tracking_Pixel.src="//pixel.everesttech.net/px2/4507__?px_evt=t&ev_" + pageName +"=1";
-		console.log("%c Fire AdobeEverestTech__Tracking_Pixel: "+"//pixel.everesttech.net/px2/4507__?px_evt=t&ev_" + pageName +"=1", "color: CornflowerBlue");
+		AdobeEverestTech__Tracking_Pixel.src="//pixel.everesttech.net/px2/4507?px_evt=t&ev_" + pageName +"=1";
+		console.log("%c AdobeEverestTech__Tracking_Pixel Fired!", log_debug_color);
 		document.body.appendChild(AdobeEverestTech__Tracking_Pixel);
 	} else {
-  		console.log("%c Fire AdobeEverestTech__Tracking_Pixel: missing parameter", "color: red");
+  		console.log("%c Fire AdobeEverestTech__Tracking_Pixel: missing parameter", log_debug_error);
   }
 }
 
@@ -37,16 +54,17 @@ function DoubleClick__Tracking_Pixel(vehicleBrand,vehicleModel,dealerID) {
 	    DoubleClick__Tracking_Pixel.alt="";
 	    DoubleClick__Tracking_Pixel.height="1";
         //
-        console.log("%c Fire DoubleClick__Tracking_Pixel: "+
+        console.log("%c DoubleClick__Tracking_Pixel Fired!", log_debug_color);
+	    document.body.appendChild(DoubleClick__Tracking_Pixel);
+    } else {
+  		console.log("%c DoubleClick__Tracking_Pixel: missing parameter: \n"+
             "//ad.doubleclick.net/ddm/activity/src=4396110;type=traffic;cat=traffic;" +
             "u1="+vehicleBrand+";"+
             "u2="+vehicleBrand+";"+
             "u3="+vehicleModel+";"+
             "u4="+dealerID+";"+
-            "dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=" + a + "?", "color: CornflowerBlue");
-	    document.body.appendChild(DoubleClick__Tracking_Pixel);
-    } else {
-  		console.log("%c DoubleClick__Tracking_Pixel: missing parameter", "color: red");
+            "dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=" + a + "?", log_debug_error
+        );
   }
 }
 
@@ -73,9 +91,9 @@ function GoogleAdwords__Conversion() {
 		}
 	}
 
-  console.log("%c Fire GoogleAdwords__Conversion: //www.googleadservices.com/pagead/conversion_async.js", "color: red");
-  var jsElm = document.createElement("script");
-	jsElm.type = "application/javascript";
+    console.log("%c GoogleAdwords__Conversion Fired!", log_debug_color);
+    var jsElm = document.createElement("script");
+    jsElm.type = "application/javascript";
 	jsElm.src = "//www.googleadservices.com/pagead/conversion_async.js";
 	document.body.appendChild(jsElm);
 }
@@ -91,6 +109,8 @@ function DoubleClick__Conversion(vehicleBrand, vehicleModel, dealershipName, pro
         ( vehicleBrand != "" )                    && ( vehicleModel != "" ) &&
         ( dealershipName != "" )                  && ( OrderID != "" ) ) {
         //
+        //
+        //
         var AdobeEverestTech__Lead_Form = new Image(1,1);
 		AdobeEverestTech__Lead_Form.src = "//ad.doubleclick.net/ddm/activity/src=4396110;type=forms;cat=leadform;qty=1;"+
 			"cost="+Revenue+";"+
@@ -101,7 +121,9 @@ function DoubleClick__Conversion(vehicleBrand, vehicleModel, dealershipName, pro
 			"u5="+ program_type+";dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;"+
 			"ord="+OrderID+"?";
         //
-  	    console.log("%c Fired DoubleClick__Conversion", "color: CornflowerBlue");
+        //
+        //
+  	    console.log("%c DoubleClick__Conversion Fired!", log_debug_color);
 		document.body.appendChild(AdobeEverestTech__Lead_Form);
 	} else {
         console.log("%c Fire DoubleClick__Conversion: missing parameters.\n"+
@@ -111,117 +133,96 @@ function DoubleClick__Conversion(vehicleBrand, vehicleModel, dealershipName, pro
 		    "u3="+ vehicleModel   +"\n"+
 		    "u4="+ dealershipName +"\n"+
 		    "u5="+ program_type+"\n"+
-		    "ord="+OrderID+"\n", "color: red"
+		    "ord="+OrderID+"\n", log_debug_error
         );
     }
 }
 
 function sd_tracking_tags() {
-    var pn = digitalData.page.pageInfo.pageName;
-    var vb = digitalData.page.pageInfo.vehicleBrand;
-    var vm = digitalData.page.pageInfo.vehicleModel;
-    var di = digitalData.page.pageInfo.dealerID;
+    //
+    pn_onChangeFn.push( function() { AdobeEverestTech__Tracking_Pixel( pn ) });
+    vb_onChangeFn.push( function() { DoubleClick__Tracking_Pixel( vb, vm, di ) } );
+    vm_onChangeFn.push( function() { DoubleClick__Tracking_Pixel( vb, vm, di ) } );
+    di_onChangeFn.push( function() { DoubleClick__Tracking_Pixel( vb, vm, di ) } );
 
     AdobeEverestTech__Tracking_Pixel( pn );
     DoubleClick__Tracking_Pixel( vb, vm, di );
+
+}
+
+function sd_conversion_tags() {
+
+    vb_onChangeFn.push( function() { DoubleClick__Conversion( vb, vm, di, pt, oi, rv ) });
+    vm_onChangeFn.push( function() { DoubleClick__Conversion( vb, vm, di, pt, oi, rv ) });
+    di_onChangeFn.push( function() { DoubleClick__Conversion( vb, vm, di, pt, oi, rv ) });
+    oi_onChangeFn.push( function() { DoubleClick__Conversion( vb, vm, di, pt, oi, rv ) });
+
+    GoogleAdwords__Conversion();
+    DoubleClick__Conversion( vb, vm, di, pt, oi, rv );
+}
     //
     //
     //
+
     if ( (typeof(digitalData.page.pageInfo.pageName)  == "undefined") || ( digitalData.page.pageInfo.vehicleModel  == "" ) ) {
-        digitalData.page.pageInfo.watch("pageName", function (id, oldval, newval) {
-            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + oldval + "' to '" + newval + "'", "color: CornflowerBlue");
-            pn = newval;
-            AdobeEverestTech__Tracking_Pixel( pn );
-            return newval;
+        digitalData.page.pageInfo.watch("pageName", function (id, ov, nv) {
+            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + ov + "' to '" + nv + "'", log_debug_color);
+            pn = nv;
+            for (var i=0; i < pn_onChangeFn.length; i++) {
+                pn_onChangeFn[i]();
+            }
+            return nv;
         });
     }
 
     if ((typeof(digitalData.page.pageInfo.vehicleBrand)  == "undefined") || ( digitalData.page.pageInfo.vehicleModel  == "" )) {
-        digitalData.page.pageInfo.watch("vehicleBrand", function (id, oldval, newval) {
-            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + oldval + "' to '" + newval + "'", "color: CornflowerBlue");
-            vb = newval
-            DoubleClick__Tracking_Pixel( vb, vm, di );
-            return newval;
+        digitalData.page.pageInfo.watch("vehicleBrand", function (id, ov, nv) {
+            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + ov + "' to '" + nv + "'", log_debug_color);
+            vb = nv
+            for (var i=0; i < vb_onChangeFn.length; i++) {
+                vb_onChangeFn[i]();
+            }
+            return nv;
           });
       }
 
     if ((typeof(digitalData.page.pageInfo.vehicleModel)  == "undefined") || ( digitalData.page.pageInfo.vehicleModel  == "" )) {
-        digitalData.page.pageInfo.watch("vehicleModel", function (id, oldval, newval) {
-            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + oldval + "' to '" + newval + "'", "color: CornflowerBlue");
-            vm = newval;
-            DoubleClick__Tracking_Pixel( vb, vm, di );
-        return newval;
+        digitalData.page.pageInfo.watch("vehicleModel", function (id, ov, nv) {
+            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + ov + "' to '" + nv + "'", log_debug_color);
+            vm = nv;
+            // DoubleClick__Tracking_Pixel( vb, vm, di );
+            for (var i=0; i < vm_onChangeFn.length; i++) {
+                vm_onChangeFn[i]();
+            }
+        return nv;
         });
     }
 
 
     if ((typeof(digitalData.page.pageInfo.dealerID)  == "undefined") || ( digitalData.page.pageInfo.dealerID  == "" )) {
-        digitalData.page.pageInfo.watch("dealerID", function (id, oldval, newval) {
-            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + oldval + "' to '" + newval + "'", "color: CornflowerBlue");
-            di = newval;
-            DoubleClick__Tracking_Pixel( vb, vb, di );
-        return newval;
+        digitalData.page.pageInfo.watch("dealerID", function (id, ov, nv) {
+            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + ov + "' to '" + nv + "'", log_debug_color);
+            di = nv;
+            for (var i=0; i < di_onChangeFn.length; i++) {
+                di_onChangeFn[i]();
+            }
+            return nv;
         });
     }
 
 
-    if ((typeof(digitalData.page.pageInfo.transactionID)  == "undefined") || ( digitalData.page.pageInfo.transactionID  == "" )) {
-        digitalData.page.pageInfo.watch("transactionID", function (id, oldval, newval) {
-            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + oldval + "' to '" + newval + "'", "color: CornflowerBlue");
-            DoubleClick__Conversion( digitalData.page.pageInfo.vehicleBrand, digitalData.page.pageInfo.vehicleModel, digitalData.page.pageInfo.dealerID, "XXX", newval, 100 );
-        return newval;
-        });
-    }
-}
-
-function sd_conversion_tags() {
-    var vb = digitalData.page.pageInfo.vehicleBrand;
-    var vm = digitalData.page.pageInfo.vehicleModel;
-    var di = digitalData.page.pageInfo.dealerID;
-    var pt = "DDA"
-    var oi = digitalData.page.pageInfo.transactionID;
-    var rv = 1;
-    GoogleAdwords__Conversion();
-    DoubleClick__Conversion( vb, vm, di, pt, oi, rv );
-    //
-    //
-    //
-    if ((typeof(digitalData.page.pageInfo.vehicleBrand)  == "undefined") || ( digitalData.page.pageInfo.vehicleModel  == "" )) {
-        digitalData.page.pageInfo.watch("vehicleBrand", function (id, oldval, newval) {
-            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + oldval + "' to '" + newval + "'", "color: CornflowerBlue");
-            vb = newval;
-            DoubleClick__Conversion( vb, vm, di, pt, oi, rv );
-            return newval;
-        });
-    }
-
-    if ((typeof(digitalData.page.pageInfo.vehicleModel)  == "undefined") || ( digitalData.page.pageInfo.vehicleModel  == "" )) {
-        digitalData.page.pageInfo.watch("vehicleModel", function (id, oldval, newval) {
-            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + oldval + "' to '" + newval + "'", "color: CornflowerBlue");
-            vm = newval;
-            DoubleClick__Conversion( vb, vm, di, pt, oi, rv );
-            return newval;
-        });
-    }
-
-    if ((typeof(digitalData.page.pageInfo.dealerID)  == "undefined") || ( digitalData.page.pageInfo.dealerID  == "" )) {
-        digitalData.page.pageInfo.watch("dealerID", function (id, oldval, newval) {
-            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + oldval + "' to '" + newval + "'", "color: CornflowerBlue");
-            di = newval;
-            DoubleClick__Conversion( vb, vm, di, pt, oi, rv );
-            return newval;
-        });
-    }
 
     if ((typeof(digitalData.page.pageInfo.transactionID)  == "undefined") || ( digitalData.page.pageInfo.transactionID  == "" )) {
-        digitalData.page.pageInfo.watch("transactionID", function (id, oldval, newval) {
-            oi = newval;
-            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + oldval + "' to '" + newval + "'", "color: CornflowerBlue");
-            DoubleClick__Conversion( vb, vm, di, pt, oi, rv );
-            return newval;
+        digitalData.page.pageInfo.watch("transactionID", function (id, ov, nv) {
+            console.log("%c digitalData.page.pageInfo." + id + " changed from: '" + ov + "' to '" + nv + "'", log_debug_color);
+            oi = nv;
+            // DoubleClick__Conversion( vb, vm, di, pt, oi, rv );
+            for ( var i=0; i < oi_onChangeFn.length; i++ ) {
+                oi_onChangeFn[i]();
+            }
+            return nv;
         });
     }
-}
 
 //
 //
